a6824abc4c6981247b4eeba23f469eabe7f7d35d
dunyo2107 <sayyoraxon.sobirjonova@gmail.com>
2024-09-01 13:04:28 +0500

    create me plan page is idealized

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
-document.getElementById("toggle-button").addEventListener("click", function() {
-    var moreText = document.getElementById("more-text");
-    var buttonText = document.getElementById("toggle-button");
-
-    if (moreText.classList.contains("hidden")) {
-        moreText.classList.remove("hidden");
-        // buttonText.textContent = "^";
-    } else {
-        moreText.classList.add("hidden");
-        // buttonText.textContent = "^";
-    }
+document.querySelectorAll(".toggle-button").forEach(function(button) {
+    button.addEventListener("click", function() {
+        var moreText = this.parentElement.nextElementSibling;
+        
+        if (moreText.classList.contains("hidden")) {
+            moreText.classList.remove("hidden");
+        } else {
+            moreText.classList.add("hidden");
+        }
+    });
 });
